8616fe70dc2615f5e751357febef2f84aceadfb2
tien2020.le2020 <tien2020.le2020@gmail.com>
2023-03-21 10:45:18 +0100

    Ensemble

--- docker-services/main.py
+++ docker-services/main.py
@@ -10,6 +10,9 @@ models_dir     = "/opt/radiant/models"
 
 
 # ----------------------------------------
+import numpy as np 
+import pandas as pd 
+import os
 
 import json
 from inference import run_inference
@@ -19,10 +22,12 @@ encoder = 'efficientnet-b0' # 'timm-efficientnet-l2'
 decoder = 'UnetPlusPlus'
 encoder_weights = 'imagenet' # 'noisy-student'
 
-
+# Meta
 with open(f'{models_dir}/models.json') as f:
 	models_json = json.load(f)
 
+# DL
+output_filenames = []
 for model_name in models_json.keys():
 	print("-"*40)
 	print(f"Running {model_name} ... ")
@@ -38,4 +43,27 @@ for model_name in models_json.keys():
 		decoder=decoder,
 		encoder_weights=encoder_weights,
 	)
+	output_filenames.append(output_filename)
 	print("-"*40)
+
+# Ensemble
+dsub = [
+    pd.read_csv(output_filename) for output_filename in output_filenames
+]
+
+n = len(dsub)
+sub = dsub[0].copy()
+
+sub.label = 0
+pred_models = []
+for d in dsub:
+    pred_models.append(d.label.apply(lambda x: int(x >= 0.27)))
+
+sub.label = np.array(pred_models).sum(0)
+sub.label = sub.label.apply(lambda x: int(x >= len(pred_models)/2))
+
+output_filename = f"{output_dir}/submission.csv"
+sub.to_csv(output_filename, index=False)
+sub.label.sum(), sub.label
+
+print(f"Final output is {output_filename} !!!")
